5deee96e9b13bc9f0366803cbf7562619e5e4aed
Roab Souza <roabsouza144@gmail.com>
2022-08-15 21:36:06 -0300

    Esse app é baseado no cronometro

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            relogio();
+        }
+
+
+        static void relogio()
+        {
+            int seg = 0;
+            int min = 0;
+            int hour = 0;
+
+            while (seg < 10)
+            {
+
+                seg++;
+                Thread.Sleep(1000);
+
+                while (seg == 10)
+                {
+                    min++;
+                    seg = 0;
+
+                    while (min == 10)
+                    {
+                        hour++;
+                        min = 0;
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine(hour + ":" + min + ":" + seg);
+
+            }
+
+
+        }
+    }
+}
